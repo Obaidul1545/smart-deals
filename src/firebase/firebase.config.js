4e7a124622af929c2ed2afaf341b5eb97f99c5de
Obaidul1545 <mdobaidulislam128@gmail.com>
2025-10-31 23:18:00 +0600

    project setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBe8Zcylfd1Kp7haZ3VGfIUkDS_4ueAIp8',
+  authDomain: 'smart-deals-dc308.firebaseapp.com',
+  projectId: 'smart-deals-dc308',
+  storageBucket: 'smart-deals-dc308.firebasestorage.app',
+  messagingSenderId: '495504053107',
+  appId: '1:495504053107:web:c2cfffdfa484654d2aab06',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
+import RootLayout from './Layouts/RootLayout.jsx';
+import Home from './components/Home/Home.jsx';
+import AllProducts from './components/AllProducts/AllProducts.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout></RootLayout>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+      {
+        path: '/allProducts',
+        element: <AllProducts></AllProducts>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
